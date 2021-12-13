7879266e17d3ebf2ecb42cbbf198a1f1c7a84821
Francis Kabiru <Kabiru>
2021-12-13 08:47:33 +0300

    Updated CarMan

--- carMan/src/main/java/com/personal/carMan/Controllers/CarAvailable.java
+++ carMan/src/main/java/com/personal/carMan/Controllers/CarAvailable.java
@@ -4,7 +4,17 @@
  */
 package com.personal.carMan.Controllers;
 
+import com.personal.carMan.Services.CarDetails;
+import com.personal.carMan.Services.CarDretailService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,10 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CarAvailable {
     
+    @Autowired
+    CarDretailService crDtls;
+            
+            
     @GetMapping("/carsAvailable")
     public String availableCars(){
         System.out.print("***RESPONDING****");
         return "No available Cars";
     }
-    
+     
+    @GetMapping("/allCars")
+    public ResponseEntity<List> findAllCars(){
+        return new ResponseEntity(crDtls.allCars(),HttpStatus.OK)  ;
+    }
+    @PostMapping("/addCar")
+    public ResponseEntity <List> createOneCar(@RequestBody CarDetails carDtls){
+         return new ResponseEntity(crDtls.createCar(carDtls),HttpStatus.OK);
+    }
 }
