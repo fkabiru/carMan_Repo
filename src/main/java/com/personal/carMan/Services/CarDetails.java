f75427425403658f521cb5900b56ae7bf31c446b
fnkabiru <fnkabiru@gmail.com>
2021-12-30 12:58:51 +0000

    add project files

---
+++ src/main/java/com/personal/carMan/Services/CarDetails.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.personal.carMan.Services;
+
+/**
+ *
+ * @author fnkab
+ */
+public class CarDetails {
+    
+    public CarDetails(){
+        
+    }
+    private String engineNo;
+    private String yearManufactured;
+
+    public String getEngineNo() {
+        return engineNo;
+    }
+
+    public void setEngineNo(String engineNo) {
+        this.engineNo = engineNo;
+    }
+
+    public String getYearManufactured() {
+        return yearManufactured;
+    }
+
+    public void setYearManufactured(String yearManufactured) {
+        this.yearManufactured = yearManufactured;
+    }
+    
+    
+}
